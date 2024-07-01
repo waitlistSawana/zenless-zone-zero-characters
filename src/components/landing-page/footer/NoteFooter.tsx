301ae837c9ff15e8ae90959973d6d48d96f1daa2
SawanaHSN <130306226+waitlistSawana@users.noreply.github.com>
2024-07-02 03:24:09 +0800

    Update NoteFooter.tsx

--- src/components/landing-page/footer/NoteFooter.tsx
+++ src/components/landing-page/footer/NoteFooter.tsx
@@ -4,7 +4,7 @@ export default function NoteFooter() {
   return (
     <div>
       <p className="mb-5 text-center text-sm opacity-50 dark:text-zinc-50">
-        @2024 @Example.com all rights reserved.
+        @2024 @zenlesszonezerocharacters.com all rights reserved.
       </p>
     </div>
   );
